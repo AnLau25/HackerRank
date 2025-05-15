4ba7933f247bee04a3670e55d39676ed4fdd0b64
AnLau25 <avald024@uottawa.ca>
2025-05-15 14:48:38 -0400

    comntd

--- flippingMatrix/main.cpp
+++ flippingMatrix/main.cpp
@@ -128,22 +128,10 @@ vector<string> split(const string &str) {
     return tokens;
 }
 
-
 /*
-Aparently XOR is a thing in c++???
-like???????
-
-int lonelyinteger(vector<int> a) {
-    int result = 0;
-    for (int num : a) {
-        result ^= num;
-    }
-    return result;
-}
-*/
-
-/* 
-Input format:
-1. 6 <length of array>
-2. 5 5 6 7 6 5 <space separated array>
+Inut format:
+1. 1
+2. 1 2 3
+   4 5 6
+   7 8 9
 */
