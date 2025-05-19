16ceb4d61bc7f87acbe9208cd065bfebc8b5bb80
AnLau25 <avald024@uottawa.ca>
2025-05-19 12:41:54 -0400

    truckTour

---
+++ truckTour/main.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'truckTour' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY petrolpumps as parameter.
+ */
+
+int truckTour(vector<vector<int>> petrolpumps) {
+    long start = 0;
+    
+    for (long i = 1; i<petrolpumps.size(); i++){
+      
+        long runable = petrolpumps[i][0] - petrolpumps[i][1];
+        long current = petrolpumps[start][0] - petrolpumps[start][1];
+        if (runable>current && petrolpumps[i][0]>petrolpumps[start][0]){
+            start = i;
+        }        
+    }
+    
+    return start;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<vector<int>> petrolpumps(n);
+
+    for (int i = 0; i < n; i++) {
+        petrolpumps[i].resize(2);
+
+        string petrolpumps_row_temp_temp;
+        getline(cin, petrolpumps_row_temp_temp);
+
+        vector<string> petrolpumps_row_temp = split(rtrim(petrolpumps_row_temp_temp));
+
+        for (int j = 0; j < 2; j++) {
+            int petrolpumps_row_item = stoi(petrolpumps_row_temp[j]);
+
+            petrolpumps[i][j] = petrolpumps_row_item;
+        }
+    }
+
+    std::cout<<truckTour(petrolpumps);
+
+    //fout << result << "\n";
+
+    //fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/*
+Aparently XOR is a thing in c++???
+like???????
+
+int lonelyinteger(vector<int> a) {
+    int result = 0;
+    for (int num : a) {
+        result ^= num;
+    }
+    return result;
+}
+*/
+
+/* 
+Input format:
+1. 6 <length of array>
+2. 5 5 6 7 6 5 <space separated array>
+*/
