706add3bdb4478e72479e3716a720d0c87ace2c8
AnLau25 <avald024@uottawa.ca>
2025-05-24 13:39:50 -0400

    cmnds

--- textEditor/main.cpp
+++ textEditor/main.cpp
@@ -7,94 +7,51 @@
 using namespace std;
 
 
-struct StackNode{
-    StackNode* next;
-    int value;
+void append(string a, vector<string>*){
     
-    StackNode(int node_value){
-        this->value = node_value;
-        this->next = nullptr;
-    }
-};
-
-class Stack {
-public:
-    StackNode* head;
-    StackNode* tail;
     
-    Stack(){
-        this->head = nullptr; 
-        this->tail = nullptr;
-    }
+}
+
+void erase(string a, vector<string>*){
     
-    void enqueue (int node_value){
-        StackNode* node = new StackNode(node_value);
-        
-        if (!this->head) {
-            this->head = node;
-        }else{
-            this->tail->next = node;
-        }
+    
+}
 
-        this->tail = node;
-        
-    }
+void print(int a, vector<string>*){
+    
+}
 
-    void dequeue (){
-        if(this->head->next){
-            this->head = this->head->next;
-        }else{
-            this->head = nullptr;
-        }
-    }
-
-    void print (){
-        if(this->head){
-            int num = this->head->value;
-            std::cout<<num<<std::endl;
-        }
-    }
-};
+void undo(int a, vector<string>*){
+    
+}
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-
-    Stack* stacked = new Stack();
     
-    vector<vector<int>> cmds;
+    vector<vector<std::string>> cmds;
+    vector<char> txt;
+    vector<string> hist(2);
     
     int counter = 0;
-    std::cin >> counter;
-    std::cin.ignore();//Makes sure cin ignores next line (since it's getline) 
-
-    while(counter-->0)
+    std::cin>>counter;
+    std::cin.ignore();
+    
+        while(counter-->0)
     {   
         std::string line;
-        std::getline(std::cin, line); //Takes a full line of code
+        std::getline(std::cin, line); 
         std::istringstream iss(line);
 
-        vector<int> com;
-        int val;
+        vector<std::string> cmd;
+        std::string val;
 
-        while (iss>>val){//Pushes the input into command
-            com.push_back(val);
+        while (iss>>val){
+            cmd.push_back(val);
         }
 
-        cmds.push_back(com);//Com to commands
-    }
-
-    for(vector<int> com : cmds)//Exhecutes the commands
-    {
-        if (com[0]==1){
-            stacked->enqueue(com[1]);
-        }else if (com[0]==2){
-            stacked->dequeue();
-        }else if (com[0]==3){
-            stacked->print();
-        }   
-    }
-    
-    
+        cmds.push_back(cmd);
+        
+        
        
     return 0;
 }
