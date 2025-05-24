e44cba886b774221f9fa05ce77d3e28ef288de62
AnLau25 <avald024@uottawa.ca>
2025-05-24 13:37:29 -0400

    textEditor

---
+++ textEditor/main.cpp
@@ -0,0 +1,114 @@
+#include <cmath>
+#include <sstream>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+struct StackNode{
+    StackNode* next;
+    int value;
+    
+    StackNode(int node_value){
+        this->value = node_value;
+        this->next = nullptr;
+    }
+};
+
+class Stack {
+public:
+    StackNode* head;
+    StackNode* tail;
+    
+    Stack(){
+        this->head = nullptr; 
+        this->tail = nullptr;
+    }
+    
+    void enqueue (int node_value){
+        StackNode* node = new StackNode(node_value);
+        
+        if (!this->head) {
+            this->head = node;
+        }else{
+            this->tail->next = node;
+        }
+
+        this->tail = node;
+        
+    }
+
+    void dequeue (){
+        if(this->head->next){
+            this->head = this->head->next;
+        }else{
+            this->head = nullptr;
+        }
+    }
+
+    void print (){
+        if(this->head){
+            int num = this->head->value;
+            std::cout<<num<<std::endl;
+        }
+    }
+};
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+    Stack* stacked = new Stack();
+    
+    vector<vector<int>> cmds;
+    
+    int counter = 0;
+    std::cin >> counter;
+    std::cin.ignore();//Makes sure cin ignores next line (since it's getline) 
+
+    while(counter-->0)
+    {   
+        std::string line;
+        std::getline(std::cin, line); //Takes a full line of code
+        std::istringstream iss(line);
+
+        vector<int> com;
+        int val;
+
+        while (iss>>val){//Pushes the input into command
+            com.push_back(val);
+        }
+
+        cmds.push_back(com);//Com to commands
+    }
+
+    for(vector<int> com : cmds)//Exhecutes the commands
+    {
+        if (com[0]==1){
+            stacked->enqueue(com[1]);
+        }else if (com[0]==2){
+            stacked->dequeue();
+        }else if (com[0]==3){
+            stacked->print();
+        }   
+    }
+    
+    
+       
+    return 0;
+}
+
+/* 
+Input format:
+1. 4 <number of commands>
+2. 1 2 <1 = enqueue number>
+3. 2 <2 = deuque from head>
+4. 1 5
+5. 3 <3 = print head>
+
+Output:
+𝘛𝘩𝘦 𝘴𝘶𝘥𝘥𝘦𝘯 𝘳𝘦𝘢𝘭𝘪𝘻𝘢𝘵𝘪𝘰𝘯 𝘵𝘩𝘢𝘵 𝘵𝘩𝘦 𝘦𝘯𝘲𝘶𝘦𝘶𝘦 𝘪𝘴 𝘢𝘭𝘭 𝘸𝘳𝘰𝘯𝘨 𝘢𝘯𝘥 𝘵𝘩𝘢𝘵 𝘵𝘩𝘪𝘴 𝘱𝘢𝘴𝘴𝘦𝘥 𝘵𝘩𝘦 𝘵𝘦𝘴𝘵𝘴 𝘣𝘺 𝘤𝘩𝘦𝘦𝘳 𝘮𝘪𝘳𝘢𝘤𝘭𝘦. Ɛ( · — ·)3
+5 <only print shows smt in terminal>
+
+*/
