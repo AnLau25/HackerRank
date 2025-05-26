5510ea3c3fabe721bf6005579b618b7e8d59a0c4
AnLau25 <avald024@uottawa.ca>
2025-05-26 11:50:18 -0400

    cookies

---
+++ cookies/main.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'cookies' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ */
+
+int cookies(int k, vector<int> A) {
+    if((A.size()==1 && A[0]<k) || A.empty()){
+        return -1;
+    }
+    
+    bool small = true;
+    int mix = 0;
+    
+    while (small) {
+        sort(A.begin(), A.end());
+        
+        if(A[0]>=k){
+            small = false;
+        }else if(A.size()==1 && A[0]<k){
+            mix = -1;
+            small = false;
+        }else{
+            int cookie1 = A[0];
+            int cookie2 = A[1];
+            
+            A.erase(A.begin(), A.begin() + 2);
+            
+            int neoCookie = cookie1 + 2*cookie2;
+        
+            A.push_back(neoCookie);
+
+            mix++;
+        }
+        
+        
+        
+        
+    }
+    
+    return mix;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string A_temp_temp;
+    getline(cin, A_temp_temp);
+
+    vector<string> A_temp = split(rtrim(A_temp_temp));
+
+    vector<int> A(n);
+
+    for (int i = 0; i < n; i++) {
+        int A_item = stoi(A_temp[i]);
+
+        A[i] = A_item;
+    }
+
+    int result = cookies(k, A);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/*
+Aparently XOR is a thing in c++???
+like???????
+
+int lonelyinteger(vector<int> a) {
+    int result = 0;
+    for (int num : a) {
+        result ^= num;
+    }
+    return result;
+}
+*/
+
+/* 
+Input format:
+1. 6 <length of array>
+2. 5 5 6 7 6 5 <space separated array>
+*/
