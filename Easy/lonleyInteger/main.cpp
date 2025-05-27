cf9aa48debf7b9230ec8896f6112dfa37fc3a481
AnLau25 <avald024@uottawa.ca>
2025-05-27 10:51:28 -0400

    oups

--- Easy/lonleyInteger/main.cpp
+++ Easy/lonleyInteger/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <string>
 #include <algorithm>  // for ltrim, rtrim
 #include <sstream>    // for split
@@ -13,108 +12,67 @@ string rtrim(const string &);
 vector<string> split(const string &);
 
 /*
- * Complete the 'bfs' function below.
+ * Complete the 'lonelyinteger' function below.
  *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER n
- *  2. INTEGER m
- *  3. 2D_INTEGER_ARRAY edges
- *  4. INTEGER s
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
  */
 
-vector<int> bfs(int n, int m, vector<vector<int>> edges, int s) {
-    vector<vector<int>> adj(n+1);
-
-    for (auto& edge : edges){
-        adj[edge[0]].push_back(edge[1]);
-        adj[edge[1]].push_back(edge[0]);
-    }
-
-    vector<int> distance(n + 1, -1);
-    vector<bool> visited(n + 1, false);
-    queue<int> q;
-
-    visited[s] = true;
-    distance[s] = 0;
-    q.push(s);
-
-    while (!q.empty()){
-        int node = q.front();
-        q.pop();
-
-        for (int neighbor : adj[node]){
-            if (!visited[neighbor]){
-                visited[neighbor] = true;
-                distance[neighbor] = distance[node] + 6;
-                q.push(neighbor);
-            }
+int lonelyinteger(vector<int> a) {
+    int len = a.size()-1;
+    bool flag = true;
+    int pringle = 0;
+    
+    while (flag) {
+        pringle = a[len];
+        
+        bool found = false;
+        for (int i = 0; i<a.size(); i++){
+            
+            if (a[i]==pringle && i!=len){
+                found = true;
+                len--;
+                break;
+            } 
+                         
         }
-    }
-
-    vector<int> paths;
-    for (int i = 1; i<= n; i++){
-        if (i!=s){
-            paths.push_back(distance[i]);
+        
+        if (!found){
+            flag = false;
         }
+        
     }
     
-    return  paths;
+    std::cout<<pringle<<std::endl;
+
+    return pringle;
 }
 
 int main()
 {
     //ofstream fout(getenv("OUTPUT_PATH"));
 
-    string q_temp;
-    getline(cin, q_temp);
+    string n_temp;
+    getline(cin, n_temp);
 
-    int q = stoi(ltrim(rtrim(q_temp)));
+    int n = stoi(ltrim(rtrim(n_temp)));
 
-    for (int q_itr = 0; q_itr < q; q_itr++) {
-        string first_multiple_input_temp;
-        getline(cin, first_multiple_input_temp);
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
 
-        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+    vector<string> a_temp = split(rtrim(a_temp_temp));
 
-        int n = stoi(first_multiple_input[0]);
+    vector<int> a(n);
 
-        int m = stoi(first_multiple_input[1]);
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
 
-        vector<vector<int>> edges(m);
-
-        for (int i = 0; i < m; i++) {
-            edges[i].resize(2);
-
-            string edges_row_temp_temp;
-            getline(cin, edges_row_temp_temp);
-
-            vector<string> edges_row_temp = split(rtrim(edges_row_temp_temp));
-
-            for (int j = 0; j < 2; j++) {
-                int edges_row_item = stoi(edges_row_temp[j]);
-
-                edges[i][j] = edges_row_item;
-            }
-        }
-
-        string s_temp;
-        getline(cin, s_temp);
-
-        int s = stoi(ltrim(rtrim(s_temp)));
-
-        vector<int> result = bfs(n, m, edges, s);
-
-        for (size_t i = 0; i < result.size(); i++) {
-            std::cout<<result[i];
-
-            if (i != result.size() - 1) {
-                std::cout<< " ";
-            }
-        }
-
-        //fout << "\n";
+        a[i] = a_item;
     }
+
+    int result = lonelyinteger(a);
+
+    //fout << result << "\n";
 
     //fout.close();
 
