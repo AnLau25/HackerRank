1a8fc886f4a02dc2fddbdb95c269e5e94209d5d3
AnLau25 <avald024@uottawa.ca>
2025-05-27 09:44:22 -0400

    BFS_ShortestReach

--- Easy/lonleyInteger/main.cpp
+++ Easy/lonleyInteger/main.cpp
@@ -12,67 +12,73 @@ string rtrim(const string &);
 vector<string> split(const string &);
 
 /*
- * Complete the 'lonelyinteger' function below.
+ * Complete the 'bfs' function below.
  *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY a as parameter.
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. 2D_INTEGER_ARRAY edges
+ *  4. INTEGER s
  */
 
-int lonelyinteger(vector<int> a) {
-    int len = a.size()-1;
-    bool flag = true;
-    int pringle = 0;
-    
-    while (flag) {
-        pringle = a[len];
-        
-        bool found = false;
-        for (int i = 0; i<a.size(); i++){
-            
-            if (a[i]==pringle && i!=len){
-                found = true;
-                len--;
-                break;
-            } 
-                         
-        }
-        
-        if (!found){
-            flag = false;
-        }
-        
-    }
-    
-    std::cout<<pringle<<std::endl;
+vector<int> bfs(int n, int m, vector<vector<int>> edges, int s) {
 
-    return pringle;
 }
 
 int main()
 {
     //ofstream fout(getenv("OUTPUT_PATH"));
 
-    string n_temp;
-    getline(cin, n_temp);
+    string q_temp;
+    getline(cin, q_temp);
 
-    int n = stoi(ltrim(rtrim(n_temp)));
+    int q = stoi(ltrim(rtrim(q_temp)));
 
-    string a_temp_temp;
-    getline(cin, a_temp_temp);
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
 
-    vector<string> a_temp = split(rtrim(a_temp_temp));
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
 
-    vector<int> a(n);
+        int n = stoi(first_multiple_input[0]);
 
-    for (int i = 0; i < n; i++) {
-        int a_item = stoi(a_temp[i]);
+        int m = stoi(first_multiple_input[1]);
 
-        a[i] = a_item;
+        vector<vector<int>> edges(m);
+
+        for (int i = 0; i < m; i++) {
+            edges[i].resize(2);
+
+            string edges_row_temp_temp;
+            getline(cin, edges_row_temp_temp);
+
+            vector<string> edges_row_temp = split(rtrim(edges_row_temp_temp));
+
+            for (int j = 0; j < 2; j++) {
+                int edges_row_item = stoi(edges_row_temp[j]);
+
+                edges[i][j] = edges_row_item;
+            }
+        }
+
+        string s_temp;
+        getline(cin, s_temp);
+
+        int s = stoi(ltrim(rtrim(s_temp)));
+
+        vector<int> result = bfs(n, m, edges, s);
+
+        for (size_t i = 0; i < result.size(); i++) {
+            std::cout<<result[i];
+
+            if (i != result.size() - 1) {
+                std::cout<< " ";
+            }
+        }
+
+        //fout << "\n";
     }
-
-    int result = lonelyinteger(a);
-
-    //fout << result << "\n";
 
     //fout.close();
 
