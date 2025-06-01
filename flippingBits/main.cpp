753164b31dbaefe89245375a489548200f138148
AnLau25 <avald024@uottawa.ca>
2025-06-01 17:06:13 -0400

    flippingBits

---
+++ flippingBits/main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+#include <bitset>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'flippingBits' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+long flippingBits(long n) {
+    std::string ed="";
+    std::string iter = std::bitset<32>(n).to_string();
+    
+    for (int i = 0; i<iter.size(); i++){
+        if (iter[i]=='0'){
+            ed+="1";
+        }
+        if (iter[i]=='1'){
+            ed+="0";
+        }
+    }
+    
+    char* endPtr;
+    long theTables= std::strtol(ed.c_str(), &endPtr, 2);
+    
+    return theTables;
+}
+
+int main()
+{
+    //ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        long n = stol(ltrim(rtrim(n_temp)));
+
+        std::cout<<flippingBits(n)<<std::endl;
+
+        //fout << result << "\n";
+    }
+
+    //fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/*
+Aparently XOR is a thing in c++???
+like???????
+
+int lonelyinteger(vector<int> a) {
+    int result = 0;
+    for (int num : a) {
+        result ^= num;
+    }
+    return result;
+}
+*/
+
+/* 
+Input format:
+1. 6 <length of array>
+2. 5 5 6 7 6 5 <space separated array>
+*/
