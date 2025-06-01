29ee50dc9210b633a47fef74233e35b901aeb9b0
AnLau25 <avald024@uottawa.ca>
2025-06-01 17:11:41 -0400

    cmntd

--- flippingBits/main.cpp
+++ flippingBits/main.cpp
@@ -104,6 +104,13 @@ vector<string> split(const string &str) {
 
 /* 
 Input format:
-1. 6 <length of array>
-2. 5 5 6 7 6 5 <space separated array>
+1. 3 <number of inputs>
+2. 2147483647 
+3. 1 
+4. 0
+
+Output:
+5. 2147483648 <flippet binary long>
+6. 4294967294 
+7. 4294967295
 */
