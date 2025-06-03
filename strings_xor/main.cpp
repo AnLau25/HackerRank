00a0388f0e67213fc3ecd60e03d4c43b2c6d1ffe
AnLau25 <avald024@uottawa.ca>
2025-06-03 10:59:14 -0400

    strings_xor(string s, string t)

--- strings_xor/main.cpp
+++ strings_xor/main.cpp
@@ -23,10 +23,10 @@ string strings_xor(string s, string t) {
     
     string res = "";
     for(int i = 0; i < s.size(); i++) {
-        if(s[i] = t[i])
-            res = '0';
+        if(s[i] == t[i])
+            res += '0';
         else
-            res = '1';
+            res += '1';
     }
     
     return res;
