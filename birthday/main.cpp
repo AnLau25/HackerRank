1b8fe62b3b3811803deaaaa029f42b9c1e7e086a
AnLau25 <avald024@uottawa.ca>
2025-06-03 10:48:35 -0400

    Ɛ( · — ·)3

--- birthday/main.cpp
+++ birthday/main.cpp
@@ -131,6 +131,10 @@ vector<string> split(const string &str) {
 
 /* 
 Input format:
-1. 6 <length of array>
-2. 5 5 6 7 6 5 <space separated array>
+1. 5 <length of array>
+2. 1 2 1 3 2 <space separated array>
+3. 3 2 <day=3, month=2>
+
+Output:
+4.2 <number of contiguous partitions>
 */
