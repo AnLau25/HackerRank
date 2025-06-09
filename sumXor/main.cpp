e1f65b038cb366625e19e58712e6e204648eab2c
AnLau25 <avald024@uottawa.ca>
2025-06-08 21:56:37 -0400

    Ɛ( · — ·)3

--- sumXor/main.cpp
+++ sumXor/main.cpp
@@ -102,6 +102,8 @@ vector<string> split(const string &str) {
 
 /* 
 Input format:
-1. 6 <length of array>
-2. 5 5 6 7 6 5 <space separated array>
+1. 10 <number to test>
+
+Output:
+2. 4 <numbers where x^10 = x+10>
 */
