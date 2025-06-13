ec0b1af0fcff80c5c49ae9c857f4e6e476bfd8be
AnLau25 <avald024@uottawa.ca>
2025-06-13 19:22:40 -0400

    bomberMan

---
+++ Medium/bomberman/main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'bomberMan' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. STRING_ARRAY grid
+ */
+
+
+
+vector<string> bomberMan(int n, vector<string> grid) {
+    if(n!=0){
+        
+    }
+    
+    return grid;
+}
+
+int main()
+{
+    //ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int r = stoi(first_multiple_input[0]);
+
+    int c = stoi(first_multiple_input[1]);
+
+    int n = stoi(first_multiple_input[2]);
+
+    vector<string> grid(r);
+
+    for (int i = 0; i < r; i++) {
+        string grid_item;
+        getline(cin, grid_item);
+
+        grid[i] = grid_item;
+    }
+
+    vector<string> result = bomberMan(n, grid);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        std::cout<< result[i];
+
+        if (i != result.size() - 1) {
+            std::cout<<"\n";
+        }
+    }
+
+    std::cout<<"\n";
+
+    //fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/* 
+Input format:
+1. 6 <length of array>
+2. 5 5 6 7 6 5 <space separated array>
+*/
