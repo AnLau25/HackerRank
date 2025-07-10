829b99647a3804e532763c17e2cc7afaccb1d4dc
AnLau25 <avald024@uottawa.ca>
2025-07-10 10:37:26 -0400

    class BadLengthException: public std::exception

---
+++ Easy/exceptionExtention/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+/* Define the exception here */
+class BadLengthException: public std::exception{
+  private:
+    string strlen;
+    
+
+  public:
+    BadLengthException(int _strlen) :strlen(to_string(_strlen)){}
+    
+    const char* what() const noexcept override{
+        return strlen.c_str();
+    }
+};
+
+
+bool checkUsername(string username) {
+	bool isValid = true;
+	int n = username.length();
+	if(n < 5) {
+		throw BadLengthException(n);
+	}
+	for(int i = 0; i < n-1; i++) {
+		if(username[i] == 'w' && username[i+1] == 'w') {
+			isValid = false;
+		}
+	}
+	return isValid;
+}
+
+int main() {
+	int T; cin >> T;
+	while(T--) {
+		string username;
+		cin >> username;
+		try {
+			bool isValid = checkUsername(username);
+			if(isValid) {
+				cout << "Valid" << '\n';
+			} else {
+				cout << "Invalid" << '\n';
+			}
+		} catch (BadLengthException e) {
+			cout << "Too short: " << e.what() << '\n';
+		}
+	}
+	return 0;
+}
+
+/*
+Intput:
+ 1. 3 <number of students>
+ 2. 30 40 45 10 10 <space separated 5 grades by student>
+ 3. 40 40 40 10 10
+ 4. 50 20 30 10 10
+
+Output:
+ 5. 1 <number of student where Student grades>Student 0 grades >
+*/
