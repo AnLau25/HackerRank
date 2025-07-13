80d76cccff98a83ef02f52885c6f1330ca23755f
AnLau25 <avald024@uottawa.ca>
2025-07-12 21:09:47 -0400

    sstreamVec

---
+++ Easy/sstreamvec/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+ 
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++) cin>>v[i];
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++) cout<<v[i]<<" ";
+    return 0;
+}
+
+
+/*
+Test:
+5
+1 6 10 8 4
+
+Output:
+1 4 6 8 10
+*/
