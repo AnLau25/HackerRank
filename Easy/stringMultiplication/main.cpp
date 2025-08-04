021eb81690bf7bd47e318aec9051be037f2a9b9d
AnLau25 <avald024@uottawa.ca>
2025-08-04 13:57:13 -0400

    tested

--- Easy/stringMultiplication/main.cpp
+++ Easy/stringMultiplication/main.cpp
@@ -13,57 +13,38 @@ string rtrim(const string &);
 vector<string> split(const string &);
 
 /*
- * Complete the 'lonelyinteger' function below.
+ * Complete the 'staircase' function below.
  *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY a as parameter.
+ * The function accepts INTEGER n as parameter.
  */
-string isValid(string s) {
-    map<char, int> charMap;
-    map<int, int> freq;
-    
-    for(char c : s){
-        charMap[c]++;
-    }
-    
-    for (auto& pair : charMap) {
-        freq[pair.second]++;
-    }
-    
-    if(freq.size()==1){
-        return "YES";
-    }
-    
-    if(freq.size()==2){
-        auto it = freq.begin();
-        int f1 = it->first, count1 = it->second;
-        ++it;
-        int f2 = it->first, count2 = it->second;
-        
-        if ((f1 == 1 && count1 == 1) || (f2 == 1 && count2 == 1)) {
-            return "YES";
-        }
 
-        if ((abs(f1 - f2) == 1) && (count1 == 1 || count2 == 1)) {
-            return "YES";
-        }
+string operator*(const std::string& str, int count){
+    std::string result = "";
+    for (int i = 0; i < count; ++i) {
+        result += str;
     }
-    
-    return "NO";
+    return result;
+}
+
+string operator*(int count, const string& str) {
+    return str * count;
+}
+
+void staircase(int n) {
+    int steps = n;
+    while (n-->0) {
+        cout<<" "s*n + "#"s*(steps-n)<<endl;
+    }
 }
 
 int main()
 {
-    //ofstream fout(getenv("OUTPUT_PATH"));
+    string n_temp;
+    getline(cin, n_temp);
 
-    string s;
-    getline(cin, s);
+    int n = stoi(ltrim(rtrim(n_temp)));
 
-    string result = isValid(s);
-
-    std::cout<<result<<"\n";
-
-    //fout.close();
+    staircase(n);
 
     return 0;
 }
