3030fdfe2e81b32612028847382cca0b0178d4e9
AnLau25 <avald024@uottawa.ca>
2025-08-13 10:28:20 -0400

    dayOfProgrammer

---
+++ Easy/dayOfProgrammer/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+#include <unordered_map>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+string dayOfProgrammer(int year) {
+    string date = "";
+    
+    
+    if((year<=1917 && (year%4==0))||
+        (year>=1919 && (year%400==0 || (year%4==0 && year%100!=0)))){
+        date = "12.09." + to_string(year);
+    }else{
+        date = "13.09." + to_string(year);
+    }
+        
+    if(year==1918){
+        date = "26.09.1918";
+    }
+    
+    return date;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string year_temp;
+    getline(cin, year_temp);
+
+    int year = stoi(ltrim(rtrim(year_temp)));
+
+    string result = dayOfProgrammer(year);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/* 
+Input format:
+1. 1 <number of test cases>
+2. 4 <saved up money>
+3. 5 <number of available flavours>
+4. 1 4 5 3 2 <spase separated array of the icecream prices>
+
+Output:
+5. 1 4 <1-based indexes of the 2 icecream flavours they should buy> 
+*/
