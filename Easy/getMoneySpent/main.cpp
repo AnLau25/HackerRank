f90f54fc7b6a6a6fd247aa686f3457bd80b0580a
AnLau25 <avald024@uottawa.ca>
2025-08-16 13:52:38 -0400

    getMoneySpent

---
+++ Easy/getMoneySpent/main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+using namespace std;
+
+vector<string> split_string(string);
+
+/*
+ * Complete the getMoneySpent function below.
+ */
+int getMoneySpent(vector<int> keyboards, vector<int> drives, int b) {
+    sort(keyboards.begin(), keyboards.end());
+    sort(drives.begin(), drives.end());
+    int inBudget = keyboards[0]+drives[0];
+    
+    if(inBudget>b) return -1;
+    if(inBudget==b) return b;
+    
+    for(int i = 0; i<keyboards.size(); i++){
+        for(int j = 0; j<drives.size(); j++){
+            int current = keyboards[i] + drives[j];
+            
+            if(current==b) return b;
+            
+            if(current<b&&current>inBudget){
+                inBudget = current;
+            }
+        }
+    }
+    
+    return inBudget;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string bnm_temp;
+    getline(cin, bnm_temp);
+
+    vector<string> bnm = split_string(bnm_temp);
+
+    int b = stoi(bnm[0]);
+
+    int n = stoi(bnm[1]);
+
+    int m = stoi(bnm[2]);
+
+    string keyboards_temp_temp;
+    getline(cin, keyboards_temp_temp);
+
+    vector<string> keyboards_temp = split_string(keyboards_temp_temp);
+
+    vector<int> keyboards(n);
+
+    for (int keyboards_itr = 0; keyboards_itr < n; keyboards_itr++) {
+        int keyboards_item = stoi(keyboards_temp[keyboards_itr]);
+
+        keyboards[keyboards_itr] = keyboards_item;
+    }
+
+    string drives_temp_temp;
+    getline(cin, drives_temp_temp);
+
+    vector<string> drives_temp = split_string(drives_temp_temp);
+
+    vector<int> drives(m);
+
+    for (int drives_itr = 0; drives_itr < m; drives_itr++) {
+        int drives_item = stoi(drives_temp[drives_itr]);
+
+        drives[drives_itr] = drives_item;
+    }
+
+    /*
+     * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+     */
+
+    int moneySpent = getMoneySpent(keyboards, drives, b);
+
+    fout << moneySpent << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
+/* 
+Input format:
+1. 1 <number of test cases>
+2. 3 <numbers of elements in list 1>
+3. 1 <enter-separated elements of list 1>
+4. 2
+5. 3
+6. 2 <numbers of elements in list 2>
+7. 1 <enter-separated elements of list 2>
+8. 2
+
+Output:
+1 1 2 2 3 4 5
+*/
