5b766777f0b7d18e0341cc412ef8021d80d58a50
AnLau25 <avald024@uottawa.ca>
2025-08-23 18:46:15 -0400

    extraLongFactorials

---
+++ Medium/extraLongFactorials/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+void extraLongFactorials(int n) {
+    int result[5000];  
+    int size = 1;   
+    result[0] = 1;
+
+    for (int x = 2; x <= n; x++) {
+        int carry = 0;
+        for (int i = 0; i < size; i++) {
+            int prod = result[i] * x + carry;
+            result[i] = prod % 10;
+            carry = prod / 10;
+        }
+        while (carry) {
+            result[size] = carry % 10;
+            carry /= 10;
+            size++;
+        }
+    }
+
+    for (int i = size - 1; i >= 0; i--) {
+        printf("%d", result[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    extraLongFactorials(n);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+1. 4 <length of array>
+2. 2 <k=2, number of integers to concider for unfairnes>
+3. 7 <numbers of the array ↓>
+4. 1
+5. 2
+6. 4
+
+Output:
+7. 1 <minimum unfairness in a sub-array of size k>
+     <[2,1]; 2-1=1 in this case>
+*/
