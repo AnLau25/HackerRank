014b9aba2a12e3fd75af5866241dbbeca57c2b8b
AnLau25 <avald024@uottawa.ca>
2025-08-28 11:08:41 -0400

    repeatedString

---
+++ Easy/repeatedString/main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+long repeatedString(string s, long n) {
+    long len = s.size();
+    long count_a = 0;
+    
+    for (char c : s){
+        if(c=='a'){
+            count_a++;
+        }
+    }
+    
+    long a_perword = (n/len)*count_a;
+    
+    if(n%len==0){
+        return a_perword;
+    } 
+    
+    for (int j = 0; j<(n%len); j++) {
+        if(s[j]=='a'){
+            a_perword++;
+        }
+    }
+    
+    return a_perword;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    long n = stol(ltrim(rtrim(n_temp)));
+
+    long result = repeatedString(s, n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+1. 6 <length of array>
+2. -2 -3 -5 0 0 1 <space separated array>
+*/
