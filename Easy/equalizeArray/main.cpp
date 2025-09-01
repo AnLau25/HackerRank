fe457e6a1211fcd19def4ac4a5e140debe1b7b22
AnLau25 <avald024@uottawa.ca>
2025-08-31 21:00:26 -0400

    tested

--- Easy/equalizeArray/main.cpp
+++ Easy/equalizeArray/main.cpp
@@ -41,7 +41,7 @@ int equalizeArray(vector<int> arr) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    //ofstream fout(getenv("OUTPUT_PATH"));
 
     string n_temp;
     getline(cin, n_temp);
@@ -63,9 +63,9 @@ int main()
 
     int result = equalizeArray(arr);
 
-    fout << result << "\n";
+    cout << result << "\n";
 
-    fout.close();
+    //fout.close();
 
     return 0;
 }
