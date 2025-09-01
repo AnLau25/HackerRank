50cebb3b04df7ff19136480ef430ba590bcd759c
AnLau25 <avald024@uottawa.ca>
2025-08-31 20:58:04 -0400

    equalizeArray

---
+++ Easy/equalizeArray/icecreamParlor/main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+#include <unordered_map>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'equalizeArray' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+int equalizeArray(vector<int> arr) {
+    sort(arr.begin(),arr.end());
+    int count = 1;
+    int temp = 1;
+    
+    for(int i = 0; i<arr.size(); i++){
+        
+        if(i+1<arr.size() && arr[i+1]==arr[i]){
+            temp++;
+        }
+        if(i+1>=arr.size() || arr[i+1]!=arr[i]){
+            count = count<temp? temp : count;
+            temp = 1;
+        }   
+        
+    }
+    
+    return arr.size()-count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = equalizeArray(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/* 
+Input format:
+1. 1 <number of test cases>
+2. 4 <saved up money>
+3. 5 <number of available flavours>
+4. 1 4 5 3 2 <spase separated array of the icecream prices>
+
+Output:
+5. 1 4 <1-based indexes of the 2 icecream flavours they should buy> 
+*/
