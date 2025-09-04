b36b84db60c084952d10657f87d92056c5ec0f7a
AnLau25 <avald024@uottawa.ca>
2025-09-04 14:04:06 -0400

    organizingContainers

---
+++ Medium/organizingContainers/main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'organizingContainers' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts 2D_INTEGER_ARRAY container as parameter.
+ */
+
+string organizingContainers(vector<vector<int>> container) {
+    int n = container.size();
+
+    vector<long long> container_capacity(n, 0);
+
+    vector<long long> ball_type_count(n, 0);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            container_capacity[i] += container[i][j];
+            ball_type_count[j] += container[i][j];
+        }
+    }
+
+    sort(container_capacity.begin(), container_capacity.end());
+    sort(ball_type_count.begin(), ball_type_count.end());
+
+    if (container_capacity == ball_type_count)
+        return "Possible";
+    else
+        return "Impossible";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        vector<vector<int>> container(n);
+
+        for (int i = 0; i < n; i++) {
+            container[i].resize(n);
+
+            string container_row_temp_temp;
+            getline(cin, container_row_temp_temp);
+
+            vector<string> container_row_temp = split(rtrim(container_row_temp_temp));
+
+            for (int j = 0; j < n; j++) {
+                int container_row_item = stoi(container_row_temp[j]);
+
+                container[i][j] = container_row_item;
+            }
+        }
+
+        string result = organizingContainers(container);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+1. 1 <number of strings to test>
+2. [{(5 5 6 7 6 5)}] <string to test>
+
+Return:
+YES <printed to terminal, NO if is not balanced, ie {[)]}>
+*/
