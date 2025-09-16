08c5fd086da7b3c9c538dc963da610d4c2896fc9
AnLau25 <avald024@uottawa.ca>
2025-09-16 12:09:23 -0400

    flatlandSpaceStations

---
+++ Easy/flatlandSpaceStations/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the flatlandSpaceStations function below.
+int flatlandSpaceStations(int n, vector<int> c) {
+    int longuest = 0;
+    
+    for(int i = 0; i<n; i++ ){
+        
+        if(find(c.begin(), c.end(), i)==c.end()){
+            int closest = *min_element(c.begin(),c.end(),
+                            [&](int a, int b){
+                                return abs(a - i) < abs(b - i);
+                            });
+            
+            longuest = abs(closest-i)>longuest? abs(closest-i) : longuest;
+        }
+        
+    }
+    
+    return longuest;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nm_temp;
+    getline(cin, nm_temp);
+
+    vector<string> nm = split_string(nm_temp);
+
+    int n = stoi(nm[0]);
+
+    int m = stoi(nm[1]);
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split_string(c_temp_temp);
+
+    vector<int> c(m);
+
+    for (int i = 0; i < m; i++) {
+        int c_item = stoi(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    int result = flatlandSpaceStations(n, c);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
+/* 
+Input format:
+ 1. 20 23 6 <range start, range end, mod>
+
+Output:
+ 2. 2 <no of beutiful days>
+*/
