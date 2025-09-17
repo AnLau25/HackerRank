2c6adeeec87579ea2add2c8962257d9fbbc3a97e
AnLau25 <avald024@uottawa.ca>
2025-09-17 10:19:45 -0400

    fairRations

---
+++ Easy/fairRations/main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'fairRations' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY B as parameter.
+ */
+
+string fairRations(vector<int> B) {
+    int breads_given = 0;
+    
+    for(int i = 0; i < B.size()-1; i++){
+        if(B[i]%2 != 0){
+            B[i+1]++;
+            breads_given+=2;
+        }
+    }
+    
+    return B[B.size()-1] % 2 == 0 ? to_string(breads_given) : "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string N_temp;
+    getline(cin, N_temp);
+
+    int N = stoi(ltrim(rtrim(N_temp)));
+
+    string B_temp_temp;
+    getline(cin, B_temp_temp);
+
+    vector<string> B_temp = split(rtrim(B_temp_temp));
+
+    vector<int> B(N);
+
+    for (int i = 0; i < N; i++) {
+        int B_item = stoi(B_temp[i]);
+
+        B[i] = B_item;
+    }
+
+    string result = fairRations(B);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+ 1. 20 23 6 <range start, range end, mod>
+
+Output:
+ 2. 2 <no of beutiful days>
+*/
