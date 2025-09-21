97fd301bc4d8776f1c7b433bd5e2db8c1e0152c4
AnLau25 <avald024@uottawa.ca>
2025-09-21 16:57:16 -0400

    strangeCounter

---
+++ Easy/strangeCounter/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'strangeCounter' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER t as parameter.
+ */
+
+long strangeCounter(long t) {
+    long cycle_length = 3;
+    long start_time = 1;
+
+    while (t >= start_time + cycle_length) {
+        start_time += cycle_length;
+        cycle_length *= 2;
+    }
+
+    return cycle_length - (t - start_time);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    long t = stol(ltrim(rtrim(t_temp)));
+
+    long result = strangeCounter(t);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+ 1. 20 23 6 <range start, range end, mod>
+
+Output:
+ 2. 2 <no of beutiful days>
+*/
