a06955a119ec0cb9bdcdc827b0823d2d3f9f2416
AnLau25 <avald024@uottawa.ca>
2025-09-23 21:45:01 -0400

    tested

--- Easy/bigSorting/main.cpp
+++ Easy/bigSorting/main.cpp
@@ -9,65 +9,58 @@ using namespace std;
 
 string ltrim(const string &);
 string rtrim(const string &);
-vector<string> split(const string &);
 
 /*
- * Complete the 'beautifulDays' function below.
+ * Complete the 'bigSorting' function below.
  *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER i
- *  2. INTEGER j
- *  3. INTEGER k
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts STRING_ARRAY unsorted as parameter.
  */
-int reverse_int(int n){
-    string sn = to_string(n);
-    
-    reverse(sn.begin(),sn.end());
-    
-    return stoi(sn);
-}
 
-int beautifulDays(int i, int j, int k) {
-    int beutiful = 0;
-    
-    while (i<=j) {
-        
-        int dif = abs(i - reverse_int(i));
-        
-        if(dif%k==0){
-            beutiful++;
-        }
-        
-        i++;
-    }
-    
-    return beutiful;
+vector<string> bigSorting(vector<string> unsorted) {
+    sort(unsorted.begin(), unsorted.end(), [](const string &a, const string &b) {
+        if (a.size() != b.size())
+            return a.size() < b.size();   
+        return a < b;        
+    });
+    return unsorted;
 }
 
 int main()
 {
     //ofstream fout(getenv("OUTPUT_PATH"));
 
-    string first_multiple_input_temp;
-    getline(cin, first_multiple_input_temp);
+    string n_temp;
+    getline(cin, n_temp);
 
-    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+    int n = stoi(ltrim(rtrim(n_temp)));
 
-    int i = stoi(first_multiple_input[0]);
+    vector<string> unsorted(n);
 
-    int j = stoi(first_multiple_input[1]);
+    for (int i = 0; i < n; i++) {
+        string unsorted_item;
+        getline(cin, unsorted_item);
 
-    int k = stoi(first_multiple_input[2]);
+        unsorted[i] = unsorted_item;
+    }
 
-    int result = beautifulDays(i, j, k);
+    vector<string> result = bigSorting(unsorted);
 
-    cout << result << "\n";
+    for (size_t i = 0; i < result.size(); i++) {
+        cout << result[i];
+
+        if (i != result.size() - 1) {
+            cout << "\n";
+        }
+    }
+
+    cout << "\n";
 
     //fout.close();
 
     return 0;
 }
+
 
 string ltrim(const string &str) {
     string s(str);
@@ -89,23 +82,6 @@ string rtrim(const string &str) {
     );
 
     return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
 
 /* 
