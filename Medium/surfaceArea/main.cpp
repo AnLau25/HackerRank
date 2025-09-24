3e291464f3f47d792b67c83889e15757f7e4f284
AnLau25 <avald024@uottawa.ca>
2025-09-24 15:16:01 -0400

    surfaceArea

---
+++ Medium/surfaceArea/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'surfaceArea' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY A as parameter.
+ */
+
+int surfaceArea(vector<vector<int>> A) {
+   int H = A.size();
+    int W = A[0].size();
+    int area = 0;
+    
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            if (A[i][j] > 0) {
+                area += 2;
+                
+                area += max(A[i][j] - (i > 0 ? A[i-1][j] : 0), 0);
+                area += max(A[i][j] - (i < H-1 ? A[i+1][j] : 0), 0);
+                area += max(A[i][j] - (j > 0 ? A[i][j-1] : 0), 0);
+                area += max(A[i][j] - (j < W-1 ? A[i][j+1] : 0), 0);
+            }
+        }
+    }
+    
+    return area;
+}
+
+int main()
+{
+    //ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int i = stoi(first_multiple_input[0]);
+
+    int j = stoi(first_multiple_input[1]);
+
+    int k = stoi(first_multiple_input[2]);
+
+    int result = beautifulDays(i, j, k);
+
+    cout << result << "\n";
+
+    //fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+ 1. 20 23 6 <range start, range end, mod>
+
+Output:
+ 2. 2 <no of beutiful days>
+*/
