c2aaf6a72e48f2d871f6fcfe55106e39702e3fc6
AnLau25 <avald024@uottawa.ca>
2025-09-25 20:39:27 -0400

    tested

--- Medium/absolutePermutation/main.cpp
+++ Medium/absolutePermutation/main.cpp
@@ -43,7 +43,7 @@ vector<int> absolutePermutation(int n, int k) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    //ofstream fout(getenv("OUTPUT_PATH"));
 
     string t_temp;
     getline(cin, t_temp);
@@ -63,17 +63,17 @@ int main()
         vector<int> result = absolutePermutation(n, k);
 
         for (size_t i = 0; i < result.size(); i++) {
-            fout << result[i];
+            cout << result[i];
 
             if (i != result.size() - 1) {
-                fout << " ";
+                cout << " ";
             }
         }
 
-        fout << "\n";
+        cout << "\n";
     }
 
-    fout.close();
+    //fout.close();
 
     return 0;
 }
