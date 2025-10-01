58ce936e8dcc40ed3efda38369de7428b8141e4d
AnLau25 <avald024@uottawa.ca>
2025-09-30 20:00:54 -0400

    larrysArray

---
+++ Medium/larrysArray/main.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'larrysArray' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY A as parameter.
+ */
+
+string larrysArray(vector<int> A) {
+    int invert = 0;
+    
+    for(int i = 0; i<A.size(); i++){
+        for(int j= i; j<A.size(); j++){
+            if(A[i] > A[j]) invert++;
+        }
+    }
+    
+    return (invert%2)==0? "YES" : "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string A_temp_temp;
+        getline(cin, A_temp_temp);
+
+        vector<string> A_temp = split(rtrim(A_temp_temp));
+
+        vector<int> A(n);
+
+        for (int i = 0; i < n; i++) {
+            int A_item = stoi(A_temp[i]);
+
+            A[i] = A_item;
+        }
+
+        string result = larrysArray(A);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/* 
+Input format:
+ 1. 6 7 3 <rows = 6, columns = 7, sec = 3>
+ 2. ....... <grid made of string arrays ↓>         
+ 3. ...O...                
+ 4. ....O..
+ 5. .......
+ 6. OO.....
+ 7. OO.....
+
+Output:
+ 8. OOO.OOO
+ 9. OO...OO
+10. OOO...O
+11. ..OO.OO
+12. ...OOOO
+13. ...OOOO
+*/
