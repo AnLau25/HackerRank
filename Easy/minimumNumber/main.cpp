a207969020771d6b98930d69a58dde8c93a105e4
AnLau25 <avald024@uottawa.ca>
2025-10-05 13:08:01 -0400

    minimumNumber

---
+++ Easy/minimumNumber/main.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'minimumNumber' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. STRING password
+ */
+
+int minimumNumber(int n, string password) {
+    // Return the minimum number of characters to make the password strong
+    int missing = 4;
+    bool num = false, low = false, upr = false, chr = false;
+    bool all = num && low && upr && chr;
+    
+    for(char c : password){
+        
+        if (!num && isdigit(c)) {
+            num = true;
+            missing--;
+        }
+        
+        if (!low && islower(c)){
+           low = true;
+           missing--; 
+        }
+        
+        if (!upr && isupper(c)){
+            upr = true;
+            missing--;
+        }
+        
+        if (!chr && (!isalnum(c) && !isspace(c))) {
+            chr = true;
+            missing--;
+        }
+        
+        if (all) return 0;      
+    }
+    
+    if (password.size()<6) return max(int(6-password.size()),missing);
+    
+    return missing;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string password;
+    getline(cin, password);
+
+    int answer = minimumNumber(n, password);
+
+    fout << answer << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+ 1. 20 23 6 <range start, range end, mod>
+
+Output:
+ 2. 2 <no of beutiful days>
+*/
