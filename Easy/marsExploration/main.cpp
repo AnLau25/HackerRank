58af211abefb44e6a6f859db86ddeedb4a1c4b57
AnLau25 <avald024@uottawa.ca>
2025-10-09 18:02:21 -0400

    tested

--- Easy/marsExploration/main.cpp
+++ Easy/marsExploration/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>  // for ltrim, rtrim
 #include <sstream>    // for split
-#include <compare>    // for spaceship operator (C++20)
 #include <iomanip> 
 using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
 
 /*
  * Complete the 'marsExploration' function below.
@@ -42,57 +36,18 @@ int marsExploration(string s) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    //ofstream fout(getenv("OUTPUT_PATH"));
 
     string s;
     getline(cin, s);
 
     int result = marsExploration(s);
 
-    fout << result << "\n";
+    cout << result << "\n";
 
-    fout.close();
+    //fout.close();
 
     return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
 
 /* 
