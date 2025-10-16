73651d03a0cb3e677bdfe686f402298e16542400
AnLau25 <avald024@uottawa.ca>
2025-10-16 14:54:17 -0400

    tested

--- Easy/countingSort2/main.cpp
+++ Easy/countingSort2/main.cpp
@@ -23,12 +23,11 @@ vector<int> countingSort(vector<int> arr) {
     for(int i = 0; i < arr.size(); i++) count[arr[i]]++;
     for(int i = 0; i < 100; i++) result.resize(result.size() + count[i], i);
     return result;
-
 }
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    //ofstream fout(getenv("OUTPUT_PATH"));
 
     string n_temp;
     getline(cin, n_temp);
@@ -51,16 +50,16 @@ int main()
     vector<int> result = countingSort(arr);
 
     for (size_t i = 0; i < result.size(); i++) {
-        fout << result[i];
+        cout << result[i];
 
         if (i != result.size() - 1) {
-            fout << " ";
+            cout << " ";
         }
     }
 
-    fout << "\n";
+    cout << "\n";
 
-    fout.close();
+    //fout.close();
 
     return 0;
 }
