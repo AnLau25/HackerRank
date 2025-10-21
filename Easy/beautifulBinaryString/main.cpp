8322753327f307ecde6e04d9ffc7e8ec47e29e37
AnLau25 <avald024@uottawa.ca>
2025-10-21 10:40:44 -0400

    beautifulBinaryString

---
+++ Easy/beautifulBinaryString/main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'beautifulBinaryString' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING b as parameter.
+ */
+
+int beautifulBinaryString(string b) {
+    int ptrn = 0;
+    
+    for (int i = 0; i<b.size(); i++){
+        string sub = b.substr(i, 3);
+        
+        if(sub=="010"){
+            ptrn++;
+            i+=2;
+        }
+    }
+    
+    return ptrn;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string b;
+    getline(cin, b);
+
+    int result = beautifulBinaryString(b);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+ 1. 20 23 6 <range start, range end, mod>
+
+Output:
+ 2. 2 <no of beutiful days>
+*/
