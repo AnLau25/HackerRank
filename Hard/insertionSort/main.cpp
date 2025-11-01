2985afe9f0973d091ab9e3768500ad62a5b25558
AnLau25 <avald024@uottawa.ca>
2025-11-01 14:36:05 -0400

    insertionSort - ussing Fenwick

---
+++ Hard/insertionSort/main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'insertionSort' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+long long insertionSort(vector<int> arr) {
+    long long count = 0, fenSum = 0;
+    static vector<int> fenwick(10000001, 0); 
+    fill(fenwick.begin(), fenwick.end(), 0);
+
+    for (int n : arr) {
+        count += fenSum;
+        int idx = n;
+        while (idx) {
+            count -= fenwick[idx];
+            idx -= idx & -idx;
+        }
+
+        idx = n;
+        while (idx < 10000001) {
+            fenwick[idx] += 1;
+            idx += idx & -idx;
+        }
+
+        fenSum += 1;
+    }
+
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+
+        vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            int arr_item = stoi(arr_temp[i]);
+
+            arr[i] = arr_item;
+        }
+
+        long long result = insertionSort(arr);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+1. 5 3 <no of elements in the array, no of queries>
+2. 1 2 100 <queries where range is {a,b} and k is the value to add>
+3. 2 5 100 <ie, a=2, b=5 & k=100>
+4. 3 4 100
+
+Output:
+5. 200 <maximum number in the array after al the sum operations>
+*/
