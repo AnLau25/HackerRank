0717cbddd0231ead32c2022875ef4a97f1ac8292
AnLau25 <avald024@uottawa.ca>
2025-11-05 16:10:28 -0500

    Ɛ( · — ·)3

--- Medium/sherlockAndAnagrams/main.cpp
+++ Medium/sherlockAndAnagrams/main.cpp
@@ -20,17 +20,17 @@ vector<string> split(const string &);
 
 int sherlockAndAnagrams(string s) {
     int ana = 0;
-    map<map<char,int>, int> grams;
+    map<map<char,int>, int> grams; // map frequency maps to their counts
     
     for (int i = 0; i < s.size(); i++) {
-        map<char,int> subgram;
+        map<char,int> subgram; // frequency map for current starting index
         for (int j = i; j < s.size(); j++) {
-            subgram[s[j]]++;   
-            grams[subgram]++; 
+            subgram[s[j]]++; // update frequency of current character   
+            grams[subgram]++;  // increment count of this frequency map
         }
     }
     
-    for(auto [substr, num] : grams) ana += (num-1) * num/2;
+    for(auto [substr, num] : grams) ana += (num-1) * num/2; // nC2 combinations of anagrammatic pairs
     
     return ana;
 }
@@ -99,12 +99,11 @@ vector<string> split(const string &str) {
 
 /* 
 Input format:
-1. 1        <number of tests = 1>
-2. 3 3 2 1  <number of cities = 3, number of city conections = 3, c_lib = 2, c_road = 1
-3. 1 2      <cities(3) = [[1, 2], [3, 1], [2, 3]]>
-4. 3 1
-5. 2 3
+ 1. 2          <no of test cases>
+ 2. ifailuhkqq <strings to test ↓>
+ 3. kkkk
 
 Output:
-6. 4 <minimum cost so all libs have a library> 
+ 4. 3          <no of anagrammatic substring pairs ↓>
+ 5. 10
 */
