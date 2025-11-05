22fa5165e03fa31fe86a2ae726adfc3add4492b1
AnLau25 <avald024@uottawa.ca>
2025-11-05 16:02:30 -0500

    sherlockAndAnagrams

---
+++ Medium/sherlockAndAnagrams/main.cpp
@@ -0,0 +1,110 @@
+#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <map>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'sherlockAndAnagrams' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int sherlockAndAnagrams(string s) {
+    int ana = 0;
+    map<map<char,int>, int> grams;
+    
+    for (int i = 0; i < s.size(); i++) {
+        map<char,int> subgram;
+        for (int j = i; j < s.size(); j++) {
+            subgram[s[j]]++;   
+            grams[subgram]++; 
+        }
+    }
+    
+    for(auto [substr, num] : grams) ana += (num-1) * num/2;
+    
+    return ana;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = sherlockAndAnagrams(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+1. 1        <number of tests = 1>
+2. 3 3 2 1  <number of cities = 3, number of city conections = 3, c_lib = 2, c_road = 1
+3. 1 2      <cities(3) = [[1, 2], [3, 1], [2, 3]]>
+4. 3 1
+5. 2 3
+
+Output:
+6. 4 <minimum cost so all libs have a library> 
+*/
