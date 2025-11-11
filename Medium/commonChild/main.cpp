9c5211e70c41822df17740594b58a4a9f320b7c4
AnLau25 <avald024@uottawa.ca>
2025-11-10 21:01:00 -0500

    v2

--- Medium/commonChild/main.cpp
+++ Medium/commonChild/main.cpp
@@ -34,6 +34,31 @@ int lcsRec(string &s1, string &s2,int m,int n, vector<vector<int>> &memo){
     return memo[m][n] = max(lcsRec(s1, s2, m, n - 1, memo), lcsRec(s1, s2, m - 1, n, memo));
 }
 
+/*Improoved with a 2d dp array
+// Returns length of LCS for s1[0..m-1], s2[0..n-1]
+int lcs(string &s1, string &s2) {
+    int m = s1.size();
+    int n = s2.size();
+
+    // Initializing a matrix of size (m+1)*(n+1)
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // Building dp[m+1][n+1] in bottom-up fashion
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    // dp[m][n] contains length of LCS for s1[0..m-1]
+    // and s2[0..n-1]
+    return dp[m][n];
+}
+*/
+
 int commonChild(string s1, string s2) {
     //2d memoization array
     //size is (m+1)x(n+1) initialized to -1
