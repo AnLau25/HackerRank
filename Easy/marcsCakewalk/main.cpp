9d8d56032f03a7e4edfc78b1fc9d141ef7b56212
AnLau25 <avald024@uottawa.ca>
2025-11-18 14:14:09 -0500

    marcsCakewalk

---
+++ Easy/marcsCakewalk/main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'marcsCakewalk' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER_ARRAY calorie as parameter.
+ */
+
+long marcsCakewalk(vector<int> calorie) {
+    sort(calorie.begin(), calorie.end(), greater<int>());
+    long miles = 0;
+    
+    for(int i = 0; i<calorie.size(); i++) miles+= pow(2, i)*calorie[i];
+    
+    return miles;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string calorie_temp_temp;
+    getline(cin, calorie_temp_temp);
+
+    vector<string> calorie_temp = split(rtrim(calorie_temp_temp));
+
+    vector<int> calorie(n);
+
+    for (int i = 0; i < n; i++) {
+        int calorie_item = stoi(calorie_temp[i]);
+
+        calorie[i] = calorie_item;
+    }
+
+    long result = marcsCakewalk(calorie);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/*
+Aparently XOR is a thing in c++???
+like???????
+
+int lonelyinteger(vector<int> a) {
+    int result = 0;
+    for (int num : a) {
+        result ^= num;
+    }
+    return result;
+}
+*/
+
+/* 
+Input format:
+1. 6 <length of array>
+2. 5 5 6 7 6 5 <space separated array>
+*/
