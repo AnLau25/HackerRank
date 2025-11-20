67164ebdae2a8fa01749d08974b3a78ab376bbc6
AnLau25 <avald024@uottawa.ca>
2025-11-19 20:30:50 -0500

    tested

--- Hard/morganAndString/main.cpp
+++ Hard/morganAndString/main.cpp
@@ -9,7 +9,6 @@ using namespace std;
 
 string ltrim(const string &);
 string rtrim(const string &);
-vector<string> split(const string &);
 
 /*
  * Complete the 'morganAndString' function below.
@@ -80,7 +79,7 @@ string morganAndString(string a, string b) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    //ofstream fout(getenv("OUTPUT_PATH"));
 
     string t_temp;
     getline(cin, t_temp);
@@ -96,10 +95,10 @@ int main()
 
         string result = morganAndString(a, b);
 
-        fout << result << "\n";
+        cout << result << "\n";
     }
 
-    fout.close();
+    //fout.close();
 
     return 0;
 }
@@ -124,23 +123,6 @@ string rtrim(const string &str) {
     );
 
     return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
 
 /* 
