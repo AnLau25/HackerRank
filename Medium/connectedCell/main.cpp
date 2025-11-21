1e4721021a34ecaa1a1bf5185061e7bbfdb68ab4
AnLau25 <avald024@uottawa.ca>
2025-11-21 18:22:33 -0500

    connectedCell

---
+++ Medium/connectedCell/main.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'connectedCell' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+ */
+
+int backtrack(vector<vector<int>> &matrix,int i,int j){
+    if(i>=0 && i<matrix.size() && j>=0 &&
+     j<matrix[0].size() && matrix[i][j]!=0){
+        matrix[i][j]--;
+        return (1+backtrack(matrix,i-1,j)+backtrack(matrix,i+1,j)+
+        backtrack(matrix,i,j-1)+backtrack(matrix,i,j+1)+backtrack(matrix,i-1,j-1)+
+        backtrack(matrix,i-1,j+1)+backtrack(matrix,i+1,j-1)+
+        backtrack(matrix,i+1,j+1));
+    }
+    else{
+        return 0;
+    }
+}
+
+
+int connectedCell(vector<vector<int>> matrix) {
+     vector<int>v;
+    for(int i=0;i<matrix.size();i++){
+        for(int j=0;j<matrix[0].size();j++){
+            if(matrix[i][j]==1){
+                v.push_back(backtrack(matrix,i,j));
+            }
+        }
+    }
+    sort(v.begin(),v.end());
+    if(v.empty()) return 0;
+    else return v[v.size()-1];
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string m_temp;
+    getline(cin, m_temp);
+
+    int m = stoi(ltrim(rtrim(m_temp)));
+
+    vector<vector<int>> matrix(n);
+
+    for (int i = 0; i < n; i++) {
+        matrix[i].resize(m);
+
+        string matrix_row_temp_temp;
+        getline(cin, matrix_row_temp_temp);
+
+        vector<string> matrix_row_temp = split(rtrim(matrix_row_temp_temp));
+
+        for (int j = 0; j < m; j++) {
+            int matrix_row_item = stoi(matrix_row_temp[j]);
+
+            matrix[i][j] = matrix_row_item;
+        }
+    }
+
+    int result = connectedCell(matrix);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+1. 7 <# of already ranked scores>
+2. 100 100 50 40 40 20 10 <space separated scores array>
+3. 4 <# of scores to rank>
+4. 5 25 50 120 <space separated array of scores>
+
+Output:
+5. 6 <rank of the latest ranked score>
+7. 4
+8. 2
+9. 1 
+*/
