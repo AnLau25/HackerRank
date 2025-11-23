358f80ea796c1c5045a19a9245ae05c083f6055d
AnLau25 <avald024@uottawa.ca>
2025-11-23 12:19:48 -0500

    maximumPerimeterTriangle

---
+++ Easy/maximumPerimeterTriangle/main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+#include <cmath>      
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'maximumPerimeterTriangle' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY sticks as parameter.
+ */
+
+vector<int> maximumPerimeterTriangle(vector<int> sticks) {
+    sort(sticks.begin(), sticks.end(), greater<int>());
+    
+    for(int i = 0; i<sticks.size()-2; i++){
+        int z = sticks[i];
+        int x = sticks[i+1];
+        int y = sticks[i+2];
+        if(z<x+y){
+            return {y, x, z};
+        }
+    }
+    
+    return {-1};
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string sticks_temp_temp;
+    getline(cin, sticks_temp_temp);
+
+    vector<string> sticks_temp = split(rtrim(sticks_temp_temp));
+
+    vector<int> sticks(n);
+
+    for (int i = 0; i < n; i++) {
+        int sticks_item = stoi(sticks_temp[i]);
+
+        sticks[i] = sticks_item;
+    }
+
+    vector<int> result = maximumPerimeterTriangle(sticks);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+ 1. 4       <length of array>
+ 2. 7 4 9 6 <space separated array>
+
+Output:
+ 3. 79      <minimum miles to burn all calories based on: 2^i * calorie[i]>
+*/
