3879bdc655219f7c2c539eb734f3315461542b7f
AnLau25 <avald024@uottawa.ca>
2025-12-02 16:27:55 -0500

    printShortestPath

---
+++ Medium/printShortestPath/main.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+#include <set>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'printShortestPath' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER i_start
+ *  3. INTEGER j_start
+ *  4. INTEGER i_end
+ *  5. INTEGER j_end
+ */
+struct cell{
+    int x, y;
+    string dir;
+    cell(): x(0), y(0), dir("") {}
+    cell(int x, int y, string dir) : x(x), y(y), dir(dir) {}
+};
+
+void printShortestPath(int n, int i_start, int j_start, int i_end, int j_end) {
+    // Print the distance along with the sequence of moves.
+    vector<cell> moves = {{-2, -1, "UL"}, {-2,  1, "UR"}, { 0,  2, "R"}, { 2,  1, "LR"}, { 2, -1, "LL"}, { 0, -2, "L"}}; 
+    int steps = -1;
+    
+    queue<cell> q;
+    vector<vector<bool>> visited(n, vector<bool> (n, false));
+    
+    visited[i_start][j_start] = true;
+    q.push({i_start, j_start, ""});
+    
+    while (!q.empty()) {
+        steps++;
+        int s = q.size();
+        while (s--) {
+            cell curr = q.front();
+            q.pop();
+            
+            if(curr.x == i_end && curr.y == j_end){
+                cout<<steps<<endl;
+                cout<<curr.dir<<endl;
+                return;
+            }
+            
+            for(cell m :  moves){
+                int nx = curr.x + m.x;
+                int ny = curr.y + m.y;
+                
+                if(nx>=0 && nx<n && ny>=0 && ny<n && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    q.push({nx, ny, curr.dir + m.dir + " "});
+                }
+            
+            }
+        }
+    }
+    
+    cout<< "Impossible" << endl;    
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int i_start = stoi(first_multiple_input[0]);
+
+    int j_start = stoi(first_multiple_input[1]);
+
+    int i_end = stoi(first_multiple_input[2]);
+
+    int j_end = stoi(first_multiple_input[3]);
+
+    printShortestPath(n, i_start, j_start, i_end, j_end);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+/* 
+Input format:
+ 1. 5 2         <arr[] size n = 5, k =2>
+ 2. 1 5 3 4 2   <arr = [1, 5, 3, 4, 2]>
+
+Output:
+ 3. 3           <no of pairs with a difference of k>
+*/
