7fef89e7a0078324b4fb7dc1fb58509c1ef46eae
AnLau25 <avald024@uottawa.ca>
2025-12-03 13:22:26 -0500

    tested

--- Easy/toys/main.cpp
+++ Easy/toys/main.cpp
@@ -38,7 +38,7 @@ int toys(vector<int> w) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    //ofstream fout(getenv("OUTPUT_PATH"));
 
     string n_temp;
     getline(cin, n_temp);
@@ -60,9 +60,9 @@ int main()
 
     int result = toys(w);
 
-    fout << result << "\n";
+    cout << result << "\n";
 
-    fout.close();
+    //fout.close();
 
     return 0;
 }
