f12895384f538fad0e2257713a53c80b4d18ff0d
AnLau25 <avald024@uottawa.ca>
2025-12-04 12:45:44 -0500

    tested

--- Easy/decentNumber/main.cpp
+++ Easy/decentNumber/main.cpp
@@ -9,7 +9,6 @@ using namespace std;
 
 string ltrim(const string &);
 string rtrim(const string &);
-vector<string> split(const string &);
 
 /*
  * Complete the 'decentNumber' function below.
@@ -66,23 +65,6 @@ string rtrim(const string &str) {
     );
 
     return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
 }
 
 /* 
