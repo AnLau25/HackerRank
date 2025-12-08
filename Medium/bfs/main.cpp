33ca0ac3dab9a637bff9752f7a35c0251faabc1e
AnLau25 <avald024@uottawa.ca>
2025-12-08 16:57:46 -0500

    bfs

---
+++ Medium/bfs/main.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>  // for ltrim, rtrim
+#include <sstream>    // for split
+#include <compare>    // for spaceship operator (C++20)
+#include <iomanip> 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'bfs' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. 2D_INTEGER_ARRAY edges
+ *  4. INTEGER s
+ */
+
+vector<int> bfs(int n, int m, vector<vector<int>> edges, int s) {
+    map<int, vector<int>> adj;
+    
+    for(vector<int> e : edges){
+        adj[e[0]-1].push_back(e[1]);
+        adj[e[1]-1].push_back(e[0]);
+    }
+    
+    queue<int> q;
+    vector<int> dist (n, -1);
+    
+    dist[s-1] = 0;
+    q.push(s);
+    
+    while (!q.empty()) {
+        int curr = q.front();
+        q.pop();
+        
+        for(int x : adj[curr-1]){
+            
+            if(dist[x-1]==-1){
+                dist[x-1] = dist[curr-1] + 6;
+                q.push(x);
+            }
+        }
+    
+    }
+    
+    dist.erase(dist.begin() + (s-1));
+    
+    return dist;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        int m = stoi(first_multiple_input[1]);
+
+        vector<vector<int>> edges(m);
+
+        for (int i = 0; i < m; i++) {
+            edges[i].resize(2);
+
+            string edges_row_temp_temp;
+            getline(cin, edges_row_temp_temp);
+
+            vector<string> edges_row_temp = split(rtrim(edges_row_temp_temp));
+
+            for (int j = 0; j < 2; j++) {
+                int edges_row_item = stoi(edges_row_temp[j]);
+
+                edges[i][j] = edges_row_item;
+            }
+        }
+
+        string s_temp;
+        getline(cin, s_temp);
+
+        int s = stoi(ltrim(rtrim(s_temp)));
+
+        vector<int> result = bfs(n, m, edges, s);
+
+        for (size_t i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
+
+/* 
+Input format:
+ 1. 6 7 3 <rows = 6, columns = 7, sec = 3>
+ 2. ....... <grid made of string arrays ↓>         
+ 3. ...O...                
+ 4. ....O..
+ 5. .......
+ 6. OO.....
+ 7. OO.....
+
+Output:
+ 8. OOO.OOO
+ 9. OO...OO
+10. OOO...O
+11. ..OO.OO
+12. ...OOOO
+13. ...OOOO
+*/
