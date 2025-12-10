8efa85fee199d6646897b081c1ef91394a729693
AnLau25 <avald024@uottawa.ca>
2025-12-10 13:12:16 -0500

    tested

--- Easy/jimOrders/main.cpp
+++ Easy/jimOrders/main.cpp
@@ -40,7 +40,7 @@ vector<int> jimOrders(vector<vector<int>> orders) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    //ofstream fout(getenv("OUTPUT_PATH"));
 
     string n_temp;
     getline(cin, n_temp);
@@ -67,16 +67,16 @@ int main()
     vector<int> result = jimOrders(orders);
 
     for (size_t i = 0; i < result.size(); i++) {
-        fout << result[i];
+        cout << result[i];
 
         if (i != result.size() - 1) {
-            fout << " ";
+            cout << " ";
         }
     }
 
-    fout << "\n";
+    cout << "\n";
 
-    fout.close();
+    //fout.close();
 
     return 0;
 }
